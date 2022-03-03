8f8a6221922c5b630760366ee2b895c221a2fc18
Azpenten <100839368+Azpenten@users.noreply.github.com>
2022-03-03 13:37:20 +0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,230 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(16, 41)
+        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(75, 22)
+        Me.RadioButton1.TabIndex = 2
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "20 мин"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 19)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Время:"
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(16, 71)
+        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(75, 22)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "25 мин"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(16, 101)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(4)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(75, 22)
+        Me.RadioButton3.TabIndex = 3
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "30 мин"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button1.Location = New System.Drawing.Point(153, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(169, 41)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Запуск"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.ForestGreen
+        Me.Label2.Location = New System.Drawing.Point(197, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 32)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "99:99"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(334, 135)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(350, 173)
+        Me.MinimumSize = New System.Drawing.Size(350, 173)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "EyeCare v1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+
+    Dim RState As Integer
+    Dim Tick As Integer
+    Dim State As Integer = 0
+    Dim MM As Integer
+    Dim SS As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If State = 0 Then
+            Timer1.Interval = Tick
+            RadioButton1.Enabled = False
+            RadioButton2.Enabled = False
+            RadioButton3.Enabled = False
+            Button1.Text = "Стоп"
+            Timer1.Start()
+            Timer2.Start()
+            State = 1
+        ElseIf State = 1 Then
+            RadioButton1.Enabled = True
+            RadioButton2.Enabled = True
+            RadioButton3.Enabled = True
+            Button1.Text = "Запуск"
+            Timer1.Stop()
+            Timer2.Stop()
+            State = 0
+            Init()
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        RState = 0
+        Tick = 20 * 60 * 1000
+        Init()
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        RState = 1
+        Tick = 25 * 60 * 1000
+        Init()
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        RState = 2
+        Tick = 30 * 60 * 1000
+        'Tick = 1000
+        Init()
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Timer2.Stop()
+        Form2.Show()
+        Tick = RState * 5 * 60 * 1000 + 20 * 60 * 1000
+        Button1_Click(sender, e)
+    End Sub
+
+    Private Sub Init()
+        MM = Tick \ 60000
+        SS = (Tick - MM * 60000) / 1000
+        If ((MM < 10) And (SS < 10)) Then
+            Label2.Text = "0" & MM & ":" & "0" & SS
+        ElseIf (SS < 10) Then
+            Label2.Text = MM & ":" & "0" & SS
+        ElseIf (MM < 10) Then
+            Label2.Text = "0" & MM & ":" & SS
+        Else
+            Label2.Text = MM & ":" & SS
+        End If
+    End Sub
+
+    Friend WithEvents Timer2 As Timer
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Tick = Tick - 1000
+        MM = Tick \ 60000
+        SS = (Tick - MM * 60000) / 1000
+        If ((MM < 10) And (SS < 10)) Then
+            Label2.Text = "0" & MM & ":" & "0" & SS
+        ElseIf (SS < 10) Then
+            Label2.Text = MM & ":" & "0" & SS
+        ElseIf (MM < 10) Then
+            Label2.Text = "0" & MM & ":" & SS
+        Else
+            Label2.Text = MM & ":" & SS
+        End If
+    End Sub
+End Class
---
+++ Form2.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play("C:\Users\Player\source\repos\EyeCare\Notify.wav", AudioPlayMode.Background)
+        Me.TopMost = True
+    End Sub
+End Class
